0339ee523082617f836276120e1fdd476aa53c6e
Voroshylo <152655013+Voroshylo@users.noreply.github.com>
2024-09-16 11:35:45 +0300

    123

--- src/server.js
+++ src/server.js
@@ -1,19 +1,15 @@
 import express from 'express';
-import cors from 'cors';
 import pino from 'pino-http';
+import cors from 'cors';
 import { env } from './utils/env.js';
-import contactsRouter from './routers/contactsRouter.js';
-import {
-  notFoundMiddleware,
-  errorHandlerMiddleware,
-} from './midllewares/index.js';
-
-const PORT = env('PORT', '3000');
-
+import { getAllContacts, getContactById } from './services/contacts.js';
+const PORT = Number(env('PORT', '3001'));
 export const setupServer = () => {
   const app = express();
 
   app.use(express.json());
+  app.use(cors());
+
   app.use(
     pino({
       transport: {
@@ -21,13 +17,52 @@ export const setupServer = () => {
       },
     }),
   );
-  app.use(cors());
 
-  app.use('/api/contacts', contactsRouter);
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Please enter /contacts for url!',
+    });
+  });
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+    console.log(contacts);
 
-  app.use(notFoundMiddleware);
+    res.status(200).json({
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
 
-  app.use(errorHandlerMiddleware);
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    // Відповідь, якщо контакт не знайдено
+    if (!contact) {
+      res.status(404).json({
+        message: 'Contact not found',
+      });
+      return;
+    }
+
+    // Відповідь, якщо контакт знайдено
+    res.status(200).json({
+      data: contact,
+    });
+  });
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
