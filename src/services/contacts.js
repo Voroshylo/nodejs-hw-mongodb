5bfe9f04241dc44df7798224c1b1cd30cceba9d3
Voroshylo <152655013+Voroshylo@users.noreply.github.com>
2024-09-18 00:03:36 +0300

    fix Id

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -5,7 +5,7 @@ export const getAllContacts = async () => {
   return contacts;
 };
 
-export const getContactById = async (contactId) => {
+export const getContactsById = async (contactId) => {
   const contact = await ContactsCollection.findById(contactId);
   return contact;
 };
