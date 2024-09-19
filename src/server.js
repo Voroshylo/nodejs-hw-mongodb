a22dc8dbab290579acbf178336dc8a0daaaa1e49
Voroshylo <152655013+Voroshylo@users.noreply.github.com>
2024-09-19 21:32:28 +0300

    fix message

--- src/server.js
+++ src/server.js
@@ -56,6 +56,7 @@ export const setupServer = () => {
 
       res.json({
         status: 200,
+        message: `Successfully found contact with id ${contactId}!`,
         data: contact,
       });
     } catch (error) {
