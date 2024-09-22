8cabd04acc4825fa1226a501200687446d16560c
Voroshylo <152655013+Voroshylo@users.noreply.github.com>
2024-09-22 19:43:18 +0300

    hw-3

---
+++ src/routers/contacts.js
@@ -0,0 +1,23 @@
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { Router } from 'express';
+import {
+  createContactController,
+  deleteContactController,
+  getContactByIdController,
+  patchContactController,
+  getContactsController,
+} from '../controllers/contacts.js';
+
+const router = Router();
+
+router.get('/', ctrlWrapper(getContactsController));
+
+router.get('/:contactId', ctrlWrapper(getContactByIdController));
+
+router.post('/', ctrlWrapper(createContactController));
+
+router.delete('/:contactId', ctrlWrapper(deleteContactController));
+
+router.patch('./:contactId', ctrlWrapper(patchContactController));
+
+export default router;
